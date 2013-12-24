899d562b21fb0b7017d6e27e9596fac148328d6b
Red-Folder <markbryantaylor@gmail.com>
2013-12-24 17:51:20 +0000

    modified:   www/myService.html 	modified:   www/myService.js

--- www/myService.js
+++ www/myService.js
@@ -14,10 +14,16 @@
  * limitations under the License.
  */
 
-cordova.require('com.red_folder.phonegap.plugin.backgroundservice.BackgroundService');
+/*
+ * Service Name
+ * This needs to be full qualified name of your service class
+ * This will be the combination of the package & class name in your service java file
+ */
+var serviceName = 'com.red_folder.phonegap.plugin.backgroundservice.sample.MyService';
 
 /*
-cordova.define(	'cordova/plugin/MyService',	CreateBackgroundService('com.red_folder.phonegap.plugin.backgroundservice.sample.MyService', require, exports, module));
-*/
-				
-CreateBackgroundService('com.red_folder.phonegap.plugin.backgroundservice.sample.MyService');
+ * Get an instance of the background service factory
+ * Use it to create a background service wrapper for your service
+ */
+var factory = require('com.red_folder.phonegap.plugin.backgroundservice.BackgroundService')
+module.exports = factory.create(serviceName);
